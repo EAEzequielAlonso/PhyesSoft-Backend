8a901699c1d1d8533b04e5843c545e16e36d6d84
EAEzequielAlonso <eze.g.alonso@gmail.com>
2024-09-24 13:11:48 -0300

    condiguracion DB, Cloudinary, JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import typeOrmConfig from './config/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeOrmConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => configService.get('typeorm'),
+    }),
+
+    // modulo para generar los token
+    JwtModule.register({
+      global: true,
+      signOptions: { expiresIn: '5h' },
+      secret: process.env.JWT_SECRET,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
